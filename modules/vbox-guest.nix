6e20721eef302f61e66a2a876ef17c504668086b
Oleg Mingalev <oleg@mingalev.net>
2023-07-04 17:19:35 +0100

    VBoxClient user systemd services

--- modules/vbox-guest.nix
+++ modules/vbox-guest.nix
@@ -1,19 +1,23 @@
 { config, pkgs, lib, ... }:
 
 {
-  # TODO: remove me after https://github.com/NixOS/nixpkgs/pull/86473 is applied
-  # services.xserver.videoDrivers = lib.mkForce [ "vmware" "virtualbox" "modesetting" ];
-  # systemd.services.virtualbox-resize = {
-  #  description = "VirtualBox Guest Screen Resizing";
+  services.xserver.videoDrivers = lib.mkForce [ "vmware" "virtualbox" "modesetting" ];
+  systemd.user.services = let
+    vbox-client = desc: flags: {
+      description = "VirtualBox Guest: ${desc}";
 
-  #  wantedBy = [ "multi-user.target" ];
-  #  requires = [ "dev-vboxguest.device" ];
-  #  after = [ "dev-vboxguest.device" ];
+      wantedBy = [ "graphical-session.target" ];
+      requires = [ "dev-vboxguest.device" ];
+      after = [ "dev-vboxguest.device" ];
 
-  #  unitConfig.ConditionVirtualization = "oracle";
+      unitConfig.ConditionVirtualization = "oracle";
 
-  #  serviceConfig.ExecStart = "${config.boot.kernelPackages.virtualboxGuestAdditions}/bin/VBoxClient -fv --vmsvga";
-  #};
+      serviceConfig.ExecStart = "${config.boot.kernelPackages.virtualboxGuestAdditions}/bin/VBoxClient -fv ${flags}";
+      };
+  in {
+    virtualbox-resize = vbox-client "Resize" "--vmsvga";
+    virtualbox-clipboard = vbox-client "Clipboard" "--clipboard";
+  };
 
   virtualisation.virtualbox.guest = {
     enable = true;
