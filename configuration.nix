7dc1c193db807fe890cf216ec7e91529b3b09bac
Oleg Mingalev <oleg@mingalev.net>
2023-07-04 14:05:04 +0100

    Configure fonts

--- configuration.nix
+++ configuration.nix
@@ -31,6 +31,18 @@
     useXkbConfig = true; # use xkbOptions in tty.
   };
 
+  fonts.fonts = with pkgs; [
+    noto-fonts
+    noto-fonts-cjk
+    noto-fonts-emoji
+    liberation_ttf
+    fira-code
+    fira-code-symbols
+    mplus-outline-fonts.githubRelease
+    dina-font
+    proggyfonts
+  ];
+
   services.xserver = {
     enable = true;
     layout = "us";
