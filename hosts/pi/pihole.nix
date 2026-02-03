0a7907c506e9a232dc8e4d9b7e7240832c509951
Oleg Mingalev <oleg@mingalev.net>
2026-02-03 17:40:19 +0000

    pi: pihole local DNS + web interface

--- hosts/pi/pihole.nix
+++ hosts/pi/pihole.nix
@@ -1,6 +1,8 @@
 { config, pkgs, lib, ... }:
 
 {
+  services.pihole-web.enable = true;
+
   services.pihole-ftl = {
     enable = true;
 
@@ -8,13 +10,20 @@
     openFirewallDNS = true;        # Port 53 for DNS
     openFirewallWebserver = true;  # Port 80 for web interface
 
-    # Upstream DNS servers
     settings = {
       dns = {
         upstreams = [
           "1.1.1.1"
           "1.0.0.1"
           "8.8.8.8"
+        ];
+        domainNeeded = true;
+        expandHosts = true;
+        domain.name = "home.mingalev.net";
+        hosts = [
+          "172.26.249.1 mingapred"
+          "172.26.249.253 pi"
+          "172.26.249.254 syslink"
         ];
       };
       webserver = {
